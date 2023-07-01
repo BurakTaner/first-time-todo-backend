1852c26bd5cf622a70f84b5b17ef42636efe6bc0
Ertan Taner <ertantaner2000@gmail.com>
2023-07-01 14:36:47 +0300

    feat: add basic healtcheck

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using TodoBackend.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using HealthChecks.UI.Client;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,11 @@ builder.Services.AddControllers().AddJsonOptions(a => a.JsonSerializerOptions.Re
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Healthcheck
+builder.Services.AddHealthChecks().AddNpgSql(builder.Configuration.GetConnectionString("TodoBackend"), tags: new string[] { "db" }).AddCheck("TodoBackend", () =>
+        HealthCheckResult.Healthy()
+        , new string[] { "main" });
 
 var app = builder.Build();
 
@@ -35,5 +43,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// map healthcheck
+app.MapHealthChecks("/healthcheck", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
